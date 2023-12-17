4b6b51737b4464ed13b1f97e83019739caa79b0e
Caden Christesen <cadenchristesen@gmail.com>
2023-12-16 19:42:12 -0600

    Initial Commit

---
+++ TabbingApp/TabbingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbingAppApp.swift
+//  TabbingApp
+//
+//  Created by Caden Christesen on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TabbingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
